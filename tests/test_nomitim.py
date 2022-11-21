e2f71576f9551b2f8153c3203219aab57ab1598d
Nizami7 <rabotahh1995@mail.ru>
2022-11-21 23:12:57 +0300

    first commit

---
+++ tests/test_nomitim.py
@@ -0,0 +1,19 @@
+import requests
+from geopandas.tools import geocode, reverse_geocode
+from shapely.geometry import Point
+
+class TestNominatim():
+    def test_lat_lng_into_location_name(self):
+        points = [Point(-71.0594869, 42.3584697), Point(-77.0365305, 38.8977332)]
+        df = reverse_geocode(points, provider="nominatim", user_agent='my_request')
+        assert df.address[
+                   0] == '18-32, Tremont Street, Downtown Crossing, Downtown Boston, Boston, Suffolk County, Massachusetts, 02108, United States'
+        assert df.address[
+                   1] == 'White House, 1600, Pennsylvania Avenue Northwest, Washington, District of Columbia, 20500, United States'
+
+    def test_location_name_into_lag_lng(self):
+        loc = 'Machu Picchu'
+        assert_point = Point(-72.54508510173372, -13.164421950000001)
+        location = geocode(loc, provider="nominatim", user_agent='my_request')
+        point = location.geometry.iloc[0]
+        assert assert_point == point
